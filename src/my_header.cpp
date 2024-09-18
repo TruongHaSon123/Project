af3597374d7d85b9f6fda00b1b6aed680120f22f
TruongHaSon123 <hason5112002@gmail.com>
2024-09-18 10:13:08 +0700

    add define

--- src/my_header.cpp
+++ src/my_header.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include "my_header.h"
 
-// Định nghĩa hàm print_message
+// Define func print_message
 void print_message() {
     std::cout << "Hello from my_header.cpp!" << std::endl;
 }
