df5fba214e55697b4095231f01986f09c3498de8
TruongHaSon123 <hason5112002@gmail.com>
2024-09-18 08:28:59 +0700

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "my_header.h"
+
+int main() {
+    print_message();
+    return 0;
+}
---
+++ src/my_header.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "my_header.h"
+
+// Định nghĩa hàm print_message
+void print_message() {
+    std::cout << "Hello from my_header.cpp!" << std::endl;
+}
