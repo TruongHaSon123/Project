b9fc31a62fc9a210b9c10e62443f36c55255a014
TruongHaSon123 <hason5112002@gmail.com>
2024-09-24 10:29:51 +0700

    change

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
-#include "my_header.h"
+#include <string>
 
-int main() {
-    print_message();
+int main(){
+    std::string name;
+    std::getline(std::cin, name);
+    name[0] = 't';
+    std::cout << "My name is: " << name << std::endl;
+    std::cout << "Length of my name is: " << name.size() << std::endl;
+    std::cout << "Final character is: " << (int)name[name.size() - 1] << std::endl;
+
+
     return 0;
 }
